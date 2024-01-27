2b98fb5e700e54ccb66df0af0caa9aebb8270d88
loganrun <bigdog_41@hotmai.com>
2024-01-26 16:46:01 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+let mainEL = document.querySelector('main');
+mainEL.style.backgroundColor = "var(--main-bg)";
+mainEL.innerHTML = '<h1>DOM Manipulation</h1>';
+mainEL.classList.add("flex-ctr")
+
+const topMenuEL = document.getElementById("top-menu");
+topMenuEL.style.height ="100%"
+topMenuEL.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEL.classList.add("flex-around")
+
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+menuLinks.forEach((i) => {
+      const link = document.createElement("a");
+      link.innerText = i.text;
+      link.href = `${i.href}`;
+      topMenuEL.appendChild(link);
+    });
+
+//Part 3
+    let subMenuEl = document.getElementById('sub-menu')
+subMenuEl.style.height = "100%"
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add('flex-around');
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top ="0";
+
+//Part 4
+
+let topMenuLinks = topMenuEL.children
+
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
+  ];
+
+//   for(link of menuLinks){
+//     console.log(link.text)
+//   }
+
+
+
+
+
+
